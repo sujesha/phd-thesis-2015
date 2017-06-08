5cd68de199122b74beb86685d10d885ed1f61a25
Sujesha Sudevalayam <sujeshass@gmail.com>
2017-06-08 16:12:15 -0700

    Included latex compile script

---
+++ make-index-nomenclature.sh
@@ -0,0 +1,18 @@
+
+makeindex thesis
+makeindex thesis.nlo  -s nomencl.ist -o thesis.nls
+bibtex thesis
+makeindex thesis
+makeindex thesis.nlo  -s nomencl.ist -o thesis.nls
+bibtex thesis
+bibtex thesis
+xelatex thesis.tex
+
+makeindex thesis
+makeindex thesis.nlo  -s nomencl.ist -o thesis.nls
+bibtex thesis
+makeindex thesis
+makeindex thesis.nlo  -s nomencl.ist -o thesis.nls
+bibtex thesis
+bibtex thesis
+xelatex thesis.tex
