3de0c15371034b001ea25981db488a18578e2c0e
Sujesha Sudevalayam <sujeshass@gmail.com>
2017-06-09 15:11:29 -0700

    Included synopsis pdf

--- make-index-nomenclature.sh
+++ make-index-nomenclature.sh
@@ -1,18 +1,34 @@
+#!/bin/sh
 
-makeindex thesis
-makeindex thesis.nlo  -s nomencl.ist -o thesis.nls
-bibtex thesis
-makeindex thesis
-makeindex thesis.nlo  -s nomencl.ist -o thesis.nls
-bibtex thesis
-bibtex thesis
-xelatex thesis.tex
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <thesis|synopsis>"
+	exit
+fi
 
-makeindex thesis
-makeindex thesis.nlo  -s nomencl.ist -o thesis.nls
-bibtex thesis
-makeindex thesis
-makeindex thesis.nlo  -s nomencl.ist -o thesis.nls
-bibtex thesis
-bibtex thesis
-xelatex thesis.tex
+if [ "$1" != "thesis" -a "$1" != "synopsis" ]
+then
+	echo "Usage: $0 <thesis|synopsis>"
+	exit
+fi
+
+name=$1
+
+
+makeindex "$name"
+makeindex "$name".nlo  -s nomencl.ist -o "$name".nls
+bibtex "$name"
+makeindex "$name"
+makeindex "$name".nlo  -s nomencl.ist -o "$name".nls
+bibtex "$name"
+bibtex "$name"
+xelatex "$name".tex
+
+makeindex "$name"
+makeindex "$name".nlo  -s nomencl.ist -o "$name".nls
+bibtex "$name"
+makeindex "$name"
+makeindex "$name".nlo  -s nomencl.ist -o "$name".nls
+bibtex "$name"
+bibtex "$name"
+xelatex "$name".tex
