4af657ab303023858c4a74e94eea0deb8a88beb4
ssudevalayam <ssudevalayam@mz.com>
2018-01-03 04:44:45 +0000

    defense ppt compiling

--- defense-ppt/make-index-nomenclature.sh
+++ defense-ppt/make-index-nomenclature.sh
@@ -1,8 +1,34 @@
+#!/bin/sh
 
-#pdflatex defense
-makeindex defense
-makeindex defense.nlo  -s nomencl.ist -o defense.nls
-bibtex defense
-makeindex defense
-makeindex defense.nlo  -s nomencl.ist -o defense.nls
-bibtex defense
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <thesis|synopsis|defense>"
+	exit
+fi
+
+if [ "$1" != "thesis" -a "$1" != "synopsis" -a "$1" != "defense" ]
+then
+	echo "Usage: $0 <thesis|synopsis|defense>"
+	exit
+fi
+
+name=$1
+
+
+makeindex "$name"
+makeindex "$name".nlo  -s nomencl.ist -o "$name".nls
+bibtex "$name"
+makeindex "$name"
+makeindex "$name".nlo  -s nomencl.ist -o "$name".nls
+bibtex "$name"
+bibtex "$name"
+xelatex "$name".tex
+
+makeindex "$name"
+makeindex "$name".nlo  -s nomencl.ist -o "$name".nls
+bibtex "$name"
+makeindex "$name"
+makeindex "$name".nlo  -s nomencl.ist -o "$name".nls
+bibtex "$name"
+bibtex "$name"
+xelatex "$name".tex
